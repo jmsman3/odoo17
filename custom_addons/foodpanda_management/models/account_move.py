fcf6ba6378eeccfbefbcee505837e46d6c1e637a
Md. Jubaer Mahmud Sarker <jmsman3@gmail.com>
2025-05-13 14:12:02 +0600

    api constraint updated

--- custom_addons/foodpanda_management/models/account_move.py
+++ custom_addons/foodpanda_management/models/account_move.py
@@ -3,8 +3,21 @@ from odoo import api , models , fields
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    appointment_id = fields.Many2one('food.order', string="Chef Appointment")
+    appointment_id = fields.Many2one('food.order', string="Chef Appointment") #this line iherits the whole objects 
 
-        
     
-  
+
+
+
+
+class AccountMoveLine(models.Model):
+    _inherit  = "account.move.line"
+
+    full_object = fields.Many2one('food.order' , string="Product Owner")
+    # food_item = fields.Char(related='full_object.food_item', string="Food name", store = True )
+
+
+
+
+    
+   
--- custom_addons/foodpanda_management/models/food.py
+++ custom_addons/foodpanda_management/models/food.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-
+from odoo.exceptions import ValidationError
 
 class FoodPanda(models.Model):
     _name = 'food.order'
@@ -29,8 +29,18 @@ class FoodPanda(models.Model):
 
     discount = fields.Float(string="Discount (%)", compute='_compute_discount')
     net_price = fields.Float(string="Net Price", compute="_compute_net_price")
-
-
+    
+    
+    #Constraint er kaaj holo Validation Handel kora
+    @api.constrains('quantity','price_per_unit')
+    def checking_quanity_valid_or_not(self):
+        if self.quantity <=0:
+            raise ValidationError("Quantity must be greater than Zero")
+        if self.price_per_unit <=0:
+            raise ValidationError("Price per Unit Must be Greater than Zero")
+        
+    
+    #THIS FUNCTION FOR DISCOUNT CALCULAION(Save data in Databse, if api depends) 
     @api.depends('quantity')
     def _compute_discount(self):
         if self.quantity>10:
@@ -38,17 +48,21 @@ class FoodPanda(models.Model):
         else:
             self.discount = 0
         
-    #This function for SET TOTAL PRICE
+    #This function for SET TOTAL PRICE(Do not saves data in Database, if api onchange use kora hoi)
     @api.onchange('total_price')
     def _compute_net_price(self):
 
         self.net_price = self.total_price * ( 1 - (self.discount/100.0))
 
-    
+    #THIS FUNCTION FOR Total Price CALCULAION(Save data in Databse, if api depends) 
     @api.depends('quantity','price_per_unit')
     def _compute_total_price(self):
-       
-        self.total_price = self.quantity * self.price_per_unit
+        print("-------------------------------------------------------------------------------")
+        print("Total price......." , self)
+        print("-------------------------------------------------------------------------------")
+        for record in self:
+
+            record.total_price = record.quantity * record.price_per_unit
         
         # print("herrrrr selfff............", self)
         # print('\n')
